328d6e384be75ddfc6ddaed3389105de265da4e3
tanjary21 <79201814+tanjary21@users.noreply.github.com>
2022-12-20 14:45:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,22 @@ from graph import *
 from max_flow import * 
 
 
-k = 5
-g = Graph()
-g.add_nodes(["s"] + [ str(i+1) for i in range(k) ] + [ f"{str(i+1)}'" for i in range(k) ] + ["t"])
 
-for i in range(k):
-    g.add_edge("s", str(i+1), {"capacity" : 2})
-    g.add_edge(f"{str(i+1)}'", "t", {"capacity" : 1})
+def run_exp(k=5, verbose=False):
+    g = Graph()
+    g.add_nodes(["s"] + [ str(i+1) for i in range(k) ] + [ f"{str(i+1)}'" for i in range(k) ] + ["t"])
 
-    for j in range(k):
-        g.add_edge(str(i+1), f"{str(i+1)}'", {"capacity" : 999999})
+    for i in range(k):
+        g.add_edge("s", str(i+1), {"capacity" : 2})
+        g.add_edge(f"{str(i+1)}'", "t", {"capacity" : 1})
+
+        for j in range(k):
+            g.add_edge(str(i+1), f"{str(j+1)}'", {"capacity" : 999999})
 
 
 
 
-print(solve_max_flow(g, g.get_node("s"), g.get_node("t"), verbose=True))
-#print(g)
+    return solve_max_flow(g, g.get_node("s"), g.get_node("t"), verbose=verbose)
+
+if __name__ == '__main__':
+    print(run_exp(k=5, verbose=False))
